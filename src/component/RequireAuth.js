47d9a8384e3e60c36d587e7585b9468cc666c246
rungruch <43019823+rungruch@users.noreply.github.com>
2023-04-19 14:57:30 +0700

    MyTicket Update

--- src/component/RequireAuth.js
+++ src/component/RequireAuth.js
@@ -1,9 +1,69 @@
-import { useLocation, Navigate, Outlet } from "react-router-dom"; import { useAuth } from "../utils/AuthProvider";
+import { useEffect, useState } from "react";
+import { useLocation, Outlet } from "react-router-dom";
+import { useAuth } from "../utils/AuthProvider";
+
+import LoginPopup from "./LoginPopup";
+import SignupPopup from "./SignupPopup";
+import Modal from "react-modal";
+
+const customStyles = {
+  overlay: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "rgba(0, 0, 0, 0.75)",
+  },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+Modal.setAppElement("#root");
+
 const RequireAuth = () => {
-const auth = useAuth();
-const location = useLocation(); console.log("*********require auth", auth.user)
-if (!auth.user?.email)
-return <Navigate to="/login" state={{ from: location }} replace />;
-  return <Outlet />
+  const auth = useAuth();
+  const location = useLocation();
+  console.log("*********require auth", auth.user);
+
+  const [isLogin, setIsLogin] = useState(true);
+  const [modalIsOpen, setIsOpen] = useState(true);
+  const [email, setEmail] = useState("");
+  function openModal() {
+    setIsOpen(true);
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  if (!auth.user?.email) {
+    return (
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+        closeModal={() => setIsOpen(false)}
+      >
+        {isLogin ? (
+          <LoginPopup
+            closeModal={closeModal}
+            setIsLogin={() => setIsLogin(false)}
+          />
+        ) : (
+          <SignupPopup
+            closeModal={closeModal}
+            setIsLogin={() => setIsLogin(true)}
+          />
+        )}
+      </Modal>
+    );
+  }
+  return <Outlet />;
 };
 export default RequireAuth;
--- src/page/MyTicket.js
+++ src/page/MyTicket.js
@@ -1,54 +1,53 @@
-import {
-	useLoaderData,
-} from 'react-router-dom'
+import { useLoaderData } from "react-router-dom";
+import "./MyTicket.css";
 
 const TicketPage = () => {
+  const products = useLoaderData();
+  const list = products.map((e) => (
+    <div className="list-content" key={e.Transactionid} to={e.Transactionid}>
+      <div className="ticket-id" title={e.category}>
+        <h1>{e.Ticketid}</h1>
+        <div className="info">
+          <p>Booking Date: {e.VisitDate}</p>
+          <p>Transaction ID: {e.Transactionid}</p>
+        </div>
+      </div>
+      <div className="quantity" title={e.category}>
+        <h2>x{e.quantity}</h2>
+      </div>
+    </div>
+  ));
 
-	const products = useLoaderData()
-	const list = products.map((e) => (
-		<div className='list-content' key={e.Transactionid} to={e.Transactionid}>
-			<div title={e.category}>{e.Ticketid}</div><br/>
-            <div title={e.category}>{e.VisitDate}</div><br/>
-            <div title={e.category}>{e.Transactionid}</div><br/>
-            <div title={e.category}>{e.quantity}</div><br/>
-		</div>
-	))
+  return (
+    <>
+      {products.length ? (
+        <div className="list-item">{list}</div>
+      ) : (
+        "No Ticket available"
+      )}
+    </>
+  );
+};
 
-	return (
-		<>
-
-			{products.length ? (
-				<div className="list-item">{list}</div>
-			) : (
-				'No Ticket available'
-			)}
-		</>
-	)
-}
-
-export default TicketPage
+export default TicketPage;
 
 export const TicketLoader = async ({ params }) => {
-	// const res = await getProducts();
-    const { id } = params
-    try {
-        const res = await fetch('/api/transactions/'+ id)
-        if (!res.ok) {
-            throw Error('Could not fetch the products')
-        }
-        return res.json();
+  // const res = await getProducts();
+  const { id } = params;
+  try {
+    const res = await fetch("/api/transactions/" + id);
+    if (!res.ok) {
+      throw Error("Could not fetch the products");
     }
-    catch (error) {
-        console.log(error);
-        return [];
-    }
-	// const res = await fetch('/api/transactions/'+ id)
-	// if (!res.ok) {
-    //     return res.json(null);
-	// 	throw Error('Could not fetch the products')
-	// }
-	// return res.json()
-}
-
-
-
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+  // const res = await fetch('/api/transactions/'+ id)
+  // if (!res.ok) {
+  //     return res.json(null);
+  // 	throw Error('Could not fetch the products')
+  // }
+  // return res.json()
+};
