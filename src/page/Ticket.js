f64122fc7a46e460f129c62b8ff3432fb71408e7
rungruch <43019823+rungruch@users.noreply.github.com>
2023-05-13 14:27:14 +0700

    add Ticket Page

--- src/page/Ticket.js
+++ src/page/Ticket.js
@@ -1,13 +1,47 @@
-import { useAuth } from "../utils/AuthProvider";
-
-const About = () => {
-  const auth = useAuth();
+import { Link, useNavigate } from "react-router-dom";
+import "../component/Ticket.css";
+const Ticket = () => {
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate("/buy");
+  }
 
   return (
     <>
-      <h1>Ticket</h1>
-      <h2>Welcome {auth.user?.name}!</h2>
+    
+    <div className="ticket-container">
+      <h1>Admission Tickets</h1>
+      <div className="ticket-content" id="Individual" onClick={handleClick}>
+          <div className="ticketbox">
+          <img src='/img/assets|ticket|indiv.jpg' alt="exhibitonimg" className="unselectable ticketimg"/>
+          <div className="ticketdetails">
+          <h2>Individual</h2>
+            <div className="ticketprice">
+            <h3>Adult</h3>
+            <h3>2,500 THB</h3>
+            </div>
+            <p>(Free if charge for child height under 90 cm.)</p>
+            <p>(Child height between 91 - 140 cm. is considered to be charged as child rate)</p>
+            <p>(Adult tickets are applied for children over 141 cm tall)</p>
+          </div>  
+          </div>
+          <div className="ticketbox" id="Family" onClick={handleClick}>
+          <img src='/img/assets|ticket|family.jpeg' alt="exhibitonimg" className="unselectable ticketimg"/>
+          <div className="ticketdetails">
+          <h2>Family</h2>
+            <div className="ticketprice">
+            <h3>2 Adults + 2 Children </h3>
+            <h3>2,500 THB</h3>
+            </div>
+            <p>(Free if charge for child height under 90 cm.)</p>
+            <p>(Child height between 91 - 140 cm. is considered to be charged as child rate)</p>
+            <p>(Adult tickets are applied for children over 141 cm tall)</p>
+          </div>
+          </div>
+
+        </div>
+      </div>
     </>
   );
 };
-export default About;
+export default Ticket;
