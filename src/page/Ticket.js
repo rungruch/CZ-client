ca9b82878ca4b795ed011fb2893ac4dfbe708f7e
rungruch <43019823+rungruch@users.noreply.github.com>
2023-05-13 22:03:23 +0700

    Update Ticket.js
    now can get ticket price dynamically

--- src/page/Ticket.js
+++ src/page/Ticket.js
@@ -1,14 +1,31 @@
 import { Link, useNavigate } from "react-router-dom";
 import "../component/Ticket.css";
+import moment from 'moment';
+import { useEffect, useState } from "react";
 const Ticket = () => {
   const navigate = useNavigate();
   function handleClick() {
     navigate("/buy");
   }
+  const [selectIndTicket, setselectIndTicket] = useState([]);
+  const [selectFamTicket, setselectFamTicket] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      //let formattedDate = moment(selectedDate.toString()).format('YYYY-MM-DD');
+      // const Inddata = await TicketLoader("Individual",formattedDate);
+      // const Famdata = await TicketLoader("Family",formattedDate);
+      const Inddata = await TicketLoader("Individual","2023-04-23");
+      const Famdata = await TicketLoader("Family","2023-04-23");
+      
+      setselectIndTicket(Inddata[0]);
+      setselectFamTicket(Famdata[0]);
+    };
+    fetchData();
+  }, []);
+
 
   return (
     <>
-    
     <div className="ticket-container">
       <h1>Admission Tickets</h1>
       <div className="ticket-content" id="Individual" onClick={handleClick}>
@@ -18,7 +35,7 @@ const Ticket = () => {
           <h2>Individual</h2>
             <div className="ticketprice">
             <h3>Adult</h3>
-            <h3>2,500 THB</h3>
+            <h3>{selectIndTicket.Price}  THB</h3>
             </div>
             <p>(Free if charge for child height under 90 cm.)</p>
             <p>(Child height between 91 - 140 cm. is considered to be charged as child rate)</p>
@@ -31,7 +48,7 @@ const Ticket = () => {
           <h2>Family</h2>
             <div className="ticketprice">
             <h3>2 Adults + 2 Children </h3>
-            <h3>2,500 THB</h3>
+            <h3>{selectFamTicket.Price} THB</h3>
             </div>
             <p>(Free if charge for child height under 90 cm.)</p>
             <p>(Child height between 91 - 140 cm. is considered to be charged as child rate)</p>
@@ -45,3 +62,13 @@ const Ticket = () => {
   );
 };
 export default Ticket;
+
+
+export const TicketLoader = async (ticketType,TDate) => {
+	const res = await fetch('/api/ticket/' + ticketType+'/'+TDate)
+	let ticket = await res.json()
+	if (!res.ok) {
+		throw Error(ticket.error)
+	}
+	return ticket
+}
